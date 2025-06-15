e54bb53449cf5c90f15fab5f5ab5db1f26ce4eea
Momoh Abdulsamad <samadmomoh@gmail.com>
2025-06-15 13:11:24 +0100

    Create story-category.tsx

---
+++ app/(home)/dashboard/_components/story-category.tsx
@@ -0,0 +1,24 @@
+import { storyCategory } from '@/data/kids';
+import StoryCard from '@/components/story-card';
+
+const StoryCategory = () => {
+  return (
+    <div className='mt-[5.25rem]'>
+      <h2 className='text-[#4A413F] text-base not-italic font-normal leading-5 font-abeezee mb-4'>
+        STORIES BY CATEGORIES
+      </h2>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+        {storyCategory.map((category) => (
+          <StoryCard
+            key={category.id}
+            img={category.img}
+            title={category.title}
+            desc={category.description}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default StoryCategory;

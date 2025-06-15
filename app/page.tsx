dc8574669e4b6655e4f87fc9e2789b5c63636d9b
Momoh Abdulsamad <samadmomoh@gmail.com>
2025-06-15 13:11:04 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import LoginPage from './(auth)/login/page';
+
 export default function Home() {
-  return <main>
-
-
-    
-  </main>;
+  return (
+    <main>
+      <LoginPage />
+    </main>
+  );
 }

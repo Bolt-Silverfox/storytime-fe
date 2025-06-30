c750071b0d2b5b5e1e74610c53f1b63528f212c3
Momoh Abdulsamad <samadmomoh@gmail.com>
2025-06-30 18:14:48 +0100

    Update kids-card.tsx

--- components/kids-card.tsx
+++ components/kids-card.tsx
@@ -3,6 +3,7 @@
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
 import { useState } from 'react';
+import edit from '@/public/edit-icon.svg';
 
 interface KidsCardProps {
   img: string;
@@ -10,6 +11,9 @@ interface KidsCardProps {
   age: string;
   cstories: string;
   active?: boolean;
+  edit?: boolean;
+  handleEdit?: () => void;
+  stories?: boolean;
 }
 
 const KidsCard = ({
@@ -18,6 +22,9 @@ const KidsCard = ({
   age,
   cstories,
   active: initialActive = false,
+  edit: initialEdit = false,
+  handleEdit,
+  stories = false,
 }: KidsCardProps) => {
   const [isActive, setIsActive] = useState(initialActive);
 
@@ -29,38 +36,50 @@ const KidsCard = ({
     <div
       onClick={handleClick}
       className={cn(
-        'border-stone-100 bg-white shadow-[0px_0px_17px_0px_rgba(34,29,29,0.05)] px-4 py-3 rounded-3xl border-[0.5px] border-solid flex items-center gap-3 cursor-pointer transition-all duration-200',
+        'border-stone-100 bg-white shadow-[0px_0px_17px_0px_rgba(34,29,29,0.05)] px-4 py-3 rounded-3xl border-[0.5px] border-solid flex items-center justify-between gap-3 cursor-pointer transition-all duration-200 w-full',
         isActive &&
           'bg-[#EC4007] shadow-[0px_0px_0px_4px_rgba(236,64,7,0.15)] rounded-3xl border-[0.5px] border-solid border-[#F84020]'
       )}
     >
-      <Image src={img} alt={name} />
-      <div className=''>
-        <p
-          className={cn(
-            'text-[#EC4007] text-xs not-italic font-normal leading-4 mb-2',
-            isActive && 'text-[#FEEAE6]'
+      <div className='flex items-center gap-3'>
+        <Image src={img} alt={name} />
+        <div className=''>
+          {stories && (
+            <p
+              className={cn(
+                'text-[#EC4007] text-xs not-italic font-normal leading-4 mb-2',
+                isActive && 'text-[#FEEAE6]'
+              )}
+            >
+              {cstories}
+            </p>
           )}
-        >
-          {cstories}
-        </p>
-        <h3
-          className={cn(
-            'text-[#221D1D] text-base not-italic font-normal leading-5',
-            isActive && 'text-white'
-          )}
-        >
-          {name}
-        </h3>
-        <p
-          className={cn(
-            'text-[#4A413F] text-xs not-italic font-normal leading-4',
-            isActive && 'text-[#FB9583]'
-          )}
-        >
-          {age}
-        </p>
+          <h3
+            className={cn(
+              'text-[#221D1D] text-base not-italic font-normal leading-5',
+              isActive && 'text-white'
+            )}
+          >
+            {name}
+          </h3>
+          <p
+            className={cn(
+              'text-[#4A413F] text-xs not-italic font-normal leading-4',
+              isActive && 'text-[#FB9583]'
+            )}
+          >
+            {age}
+          </p>
+        </div>
       </div>
+      {edit && (
+        <Image
+          src={edit}
+          alt='edit'
+          onClick={handleEdit}
+          className='cursor-pointer'
+        />
+      )}
     </div>
   );
 };

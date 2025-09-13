72daac55eec2a21ff70c179aa061d7dee50c3f92
Abdul.tsx <samadmomoh@gmail.com>
2025-09-13 01:51:30 +0100

    Update auth-footer.tsx

--- app/(auth)/register/components/auth-footer.tsx
+++ app/(auth)/register/components/auth-footer.tsx
@@ -4,11 +4,14 @@ export const AuthFooter = () => {
   return (
     <footer className='max-w-[437px] absolute -translate-x-1/2 left-1/2 bottom-10 text-center mx-auto text-xs font-abeezee '>
       By accepting to continue, you agree to storytime's{' '}
-      <Link href='#' className='text-[#0731EC] hover:underline'>
+      <Link
+        href='/terms-and-conditions'
+        className='text-[#0731EC] hover:underline'
+      >
         Terms and Conditions
       </Link>{' '}
       and{' '}
-      <Link href='#' className='text-[#0731EC] hover:underline'>
+      <Link href='/privacy-policy' className='text-[#0731EC] hover:underline'>
         Privacy Policy
       </Link>
     </footer>
